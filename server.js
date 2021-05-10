c5df2ef4967b3b3d60fa65fe6c8584552ce9ae0f
Eyal Dar <eyaldar2014@gmail.com>
2021-05-10 13:58:34 +0300

    check deployment

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
   // Express serve up index.html file if it doesn't recognize route
-  // const path = require('path');
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  // });
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 
