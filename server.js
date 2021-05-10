ad7e3ea1c77e321c8c619543cd45b16e0a84ca61
Eyal Dar <eyaldar2014@gmail.com>
2021-05-10 15:32:38 +0300

    check deployment

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const app = express();
 const port = 5000; 
 
+const os = require('os');
+const path = require('path');
+
 app.use(cors());
 
 app.get('/', function (req, res) {
@@ -14,6 +17,12 @@ app.get('/', function (req, res) {
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
+// Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
 }
 
 
