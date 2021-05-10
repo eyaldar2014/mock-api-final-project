4d2112e20e1a605c1796ead5307a44463cf225ce
Eyal Dar <eyaldar2014@gmail.com>
2021-05-10 13:58:03 +0300

    check deployment

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ app.use('/', function (req, res, next) {
   console.log('Request Type:', req.method)
   res.send('lalala')
 })
+// app.get('/api/getUser', (req, res) => res.send({ username: os.userInfo().username }));
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+  // Express serve up index.html file if it doesn't recognize route
+  // const path = require('path');
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
+}
 
 
 
