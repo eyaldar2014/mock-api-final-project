48f249c83116249ea2832a553c19f1eb540d2d08
Eyal Dar <eyaldar2014@gmail.com>
2021-08-09 17:47:45 +0300

    server registration & login

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const usersRoute = require('./routes/route.users')
 app.use(express.json())
 app.use(cors());
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/api', (req, res) => res.send('hello world'))
 app.use('/api/users',usersRoute);
 
 
